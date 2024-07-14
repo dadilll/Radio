a774efb8e6a22dcd8ada30635af30e09ede5978a
Dadilll <Danil2ka2142@yandex.ru>
2024-07-14 22:51:44 +0700

    - Improved interface. - Added the ability to add stations to favorites.

--- go.mod
+++ go.mod
@@ -4,15 +4,30 @@ go 1.21.9
 
 require github.com/faiface/beep v1.1.0
 
-require github.com/gordonklaus/portaudio v0.0.0-20230709114228-aafa478834f5 // indirect
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/icza/bitio v1.0.0 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
+	github.com/jfreymuth/vorbis v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mewkiz/flac v1.0.7 // indirect
+	github.com/mewkiz/pkg v0.0.0-20190919212034-518ade7978e2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/term v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
 
 require (
+	github.com/gdamore/tcell v1.4.0
+	github.com/gdamore/tcell/v2 v2.7.4 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/hajimehoshi/oto v1.0.1 // indirect
+	github.com/moutend/go-equalizer v0.1.0
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/tosone/minimp3 v1.0.2
+	github.com/rivo/tview v0.0.0-20240625185742-b0a7293b8130
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
-	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+	golang.org/x/sys v0.17.0 // indirect
 )
--- radio.go
+++ radio.go
@@ -1,35 +1,71 @@
 package main
 
 import (
-	"bufio"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
+	"time"
 
-	"github.com/gordonklaus/portaudio"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
+	"github.com/faiface/beep/flac"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/vorbis"
+	"github.com/faiface/beep/wav"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
-// Station represents a radio station with name and URL.
 type Station struct {
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
 
-// fetchStations fetches radio stations from the radio-browser.info API.
+const favoritesFile = "favorites.json"
+
+func loadFavoriteStations() ([]Station, error) {
+	if _, err := os.Stat(favoritesFile); os.IsNotExist(err) {
+		return nil, fmt.Errorf("файл избранного не существует")
+	}
+	data, err := os.ReadFile(favoritesFile)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось прочитать файл избранного: %v", err)
+	}
+	var stations []Station
+	err = json.Unmarshal(data, &stations)
+	if err != nil {
+		return nil, fmt.Errorf("не удалось разобрать данные избранного: %v", err)
+	}
+	return stations, nil
+}
+
+func saveStationsToFavorites(stations []Station) error {
+	data, err := json.Marshal(stations)
+	if err != nil {
+		return fmt.Errorf("не удалось преобразовать станции: %v", err)
+	}
+	err = os.WriteFile(favoritesFile, data, 0644)
+	if err != nil {
+		return fmt.Errorf("не удалось записать файл избранного: %v", err)
+	}
+	return nil
+}
+
 func fetchStations() ([]Station, error) {
 	resp, err := http.Get("https://de1.api.radio-browser.info/json/stations/topclick/50")
 	if err != nil {
-		return nil, fmt.Errorf("could not fetch stations: %v", err)
+		return nil, fmt.Errorf("не удалось получить станции: %v", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("bad status: %s", resp.Status)
+		return nil, fmt.Errorf("плохой статус: %s", resp.Status)
 	}
 
 	var data []struct {
@@ -49,59 +85,52 @@ func fetchStations() ([]Station, error) {
 	return stations, nil
 }
 
+func isStreamAvailable(url string) bool {
+	cmd := exec.Command("ffmpeg", "-i", url, "-t", "5", "-f", "null", "-")
+	err := cmd.Run()
+	return err == nil
+}
+
+func updateStationsPeriodically(interval time.Duration, stations *[]Station) {
+	ticker := time.NewTicker(interval)
+	for {
+		<-ticker.C
+		newStations, err := fetchStations()
+		if err == nil {
+			*stations = newStations
+		}
+	}
+}
+
+var volume = &effects.Volume{
+	Base:   2,
+	Volume: 0,
+	Silent: false,
+}
+
 func playStream(url string, control chan string, wg *sync.WaitGroup) error {
 	defer wg.Done()
 
-	cmd := exec.Command("ffmpeg", "-loglevel", "error", "-i", url, "-f", "s16le", "-ar", "44100", "-ac", "2", "pipe:1")
-	stdout, err := cmd.StdoutPipe()
+	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("could not get stdout pipe: %v", err)
+		return fmt.Errorf("не удалось получить поток: %v", err)
 	}
-	stderr, err := cmd.StderrPipe()
+	defer resp.Body.Close()
+
+	streamer, format, err := decodeAudio(resp.Body)
 	if err != nil {
-		return fmt.Errorf("could not get stderr pipe: %v", err)
-	}
-	if err := cmd.Start(); err != nil {
-		return fmt.Errorf("could not start ffmpeg: %v", err)
+		return fmt.Errorf("не удалось декодировать аудио: %v", err)
 	}
 
-	go func() {
-		scanner := bufio.NewScanner(stderr)
-		for scanner.Scan() {
-			fmt.Fprintln(os.Stderr, scanner.Text())
-		}
-	}()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-
-	stream, err := portaudio.OpenDefaultStream(0, 2, 44100, 0, func(out []float32) {
-		buf := make([]byte, len(out)*2)
-		_, err := io.ReadFull(stdout, buf)
-		if err != nil {
-			for i := range out {
-				out[i] = 0
-			}
-			return
-		}
-		for i := 0; i < len(out)/2; i++ {
-			out[2*i] = float32(int16(binary.LittleEndian.Uint16(buf[4*i:]))) / 32768.0
-			out[2*i+1] = float32(int16(binary.LittleEndian.Uint16(buf[4*i+2:]))) / 32768.0
-		}
-	})
-	if err != nil {
-		return fmt.Errorf("could not open default stream: %v", err)
-	}
-	defer stream.Close()
-
-	if err := stream.Start(); err != nil {
-		return fmt.Errorf("could not start stream: %v", err)
-	}
+	volume.Streamer = streamer
+	speaker.Play(volume)
 
 	done := make(chan bool)
 	go func() {
 		<-done
-		cmd.Process.Kill()
+		resp.Body.Close()
 	}()
 
 	for {
@@ -109,22 +138,64 @@ func playStream(url string, control chan string, wg *sync.WaitGroup) error {
 		case cmd := <-control:
 			switch cmd {
 			case "pause":
-				if err := stream.Stop(); err != nil {
-					return fmt.Errorf("could not stop stream: %v", err)
-				}
+				speaker.Lock()
+				volume.Silent = true
+				speaker.Unlock()
 			case "resume":
-				if err := stream.Start(); err != nil {
-					return fmt.Errorf("could not start stream: %v", err)
-				}
+				speaker.Lock()
+				volume.Silent = false
+				speaker.Unlock()
 			case "stop":
 				done <- true
-				if err := stream.Stop(); err != nil {
-					return fmt.Errorf("could not stop stream: %v", err)
-				}
 				return nil
+			case "volume_up":
+				speaker.Lock()
+				volume.Volume += 0.1
+				speaker.Unlock()
+			case "volume_down":
+				speaker.Lock()
+				volume.Volume -= 0.1
+				speaker.Unlock()
 			}
 		}
 	}
+}
+
+type readCloser struct {
+	io.Reader
+}
+
+func (rc readCloser) Close() error {
+	return nil
+}
+
+func decodeAudio(r io.Reader) (beep.StreamSeekCloser, beep.Format, error) {
+	rc := readCloser{r}
+	var streamer beep.StreamSeekCloser
+	var format beep.Format
+	var err error
+
+	if streamer, format, err = mp3.Decode(rc); err == nil {
+		return streamer, format, nil
+	}
+	if streamer, format, err = flac.Decode(rc); err == nil {
+		return streamer, format, nil
+	}
+	if streamer, format, err = wav.Decode(rc); err == nil {
+		return streamer, format, nil
+	}
+	if streamer, format, err = vorbis.Decode(rc); err == nil {
+		return streamer, format, nil
+	}
+
+	return nil, beep.Format{}, fmt.Errorf("неподдерживаемый формат")
+}
+
+func updateEqualizer(equalizer *tview.TextView, volumeLevel float64, stationName string) {
+	barCount := 10
+	fullBars := int(volumeLevel * float64(barCount))
+	bars := strings.Repeat("[green]█", fullBars) + strings.Repeat("[grey]█", barCount-fullBars)
+	equalizer.SetText(fmt.Sprintf("Громкость: %.1f\nСтанция: %s\n%s", volumeLevel, stationName, bars))
 }
 
 func main() {
@@ -134,25 +205,56 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Доступные станции:")
-	for i, station := range stations {
-		fmt.Printf("%d: %s\n", i+1, station.Name)
+	favorites, err := loadFavoriteStations()
+	if err != nil {
+		favorites = []Station{}
 	}
 
-	var stationIndex int
-	for {
-		fmt.Print("Выберите номер станции для прослушивания: ")
-		_, err = fmt.Scan(&stationIndex)
-		if err != nil || stationIndex < 1 || stationIndex > len(stations) {
-			fmt.Println("Неверный номер станции.")
-			continue
+	app := tview.NewApplication()
+	list := tview.NewList()
+	list.SetTitle("Доступные станции").SetBorder(true)
+	for i, station := range stations {
+		list.AddItem(station.Name, "", rune('0'+i%10), nil)
+	}
+
+	favoritesList := tview.NewList()
+	favoritesList.SetTitle("Избранные станции").SetBorder(true)
+	for i, station := range favorites {
+		favoritesList.AddItem(station.Name, "", rune('0'+i%10), nil)
+	}
+
+	info := tview.NewTextView().SetText("Выберите станцию для прослушивания").SetDynamicColors(true).SetRegions(true).SetWrap(true)
+	info.SetBorder(true).SetTitle("Информация")
+
+	equalizer := tview.NewTextView().SetDynamicColors(true).SetWrap(true)
+	equalizer.SetBorder(true).SetTitle("Эквалайзер")
+
+	currentStation := ""
+	updateEqualizer(equalizer, volume.Volume, currentStation)
+
+	flex := tview.NewFlex().
+		AddItem(list, 0, 1, true).
+		AddItem(info, 0, 2, false).
+		AddItem(equalizer, 0, 2, false)
+
+	var control chan string
+	var wg sync.WaitGroup
+
+	list.SetChangedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		info.SetText(fmt.Sprintf("Станция: %s\nURL: %s", stations[index].Name, stations[index].URL))
+	})
+
+	list.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		selectedStation := stations[index]
+		if !isStreamAvailable(selectedStation.URL) {
+			info.SetText(fmt.Sprintf("[red]Станция недоступна: %s", selectedStation.Name))
+			return
 		}
 
-		selectedStation := stations[stationIndex-1]
-		fmt.Printf("Вы выбрали станцию: %s\n", selectedStation.Name)
+		currentStation = selectedStation.Name
+		updateEqualizer(equalizer, volume.Volume, currentStation)
 
-		control := make(chan string)
-		var wg sync.WaitGroup
+		control = make(chan string)
 		wg.Add(1)
 		go func() {
 			err := playStream(selectedStation.URL, control, &wg)
@@ -161,29 +263,86 @@ func main() {
 			}
 		}()
 
-		exitInnerLoop := false
-		for !exitInnerLoop {
-			fmt.Println("Введите команду (pause/resume/stop/change): ")
-			var cmd string
-			_, err := fmt.Scan(&cmd)
+		info.SetText(fmt.Sprintf("[green]Играет: %s", selectedStation.Name))
+	})
+
+	favoritesList.SetSelectedFunc(func(index int, mainText string, secondaryText string, shortcut rune) {
+		selectedStation := favorites[index]
+		if !isStreamAvailable(selectedStation.URL) {
+			info.SetText(fmt.Sprintf("[red]Станция недоступна: %s", selectedStation.Name))
+			return
+		}
+
+		currentStation = selectedStation.Name
+		updateEqualizer(equalizer, volume.Volume, currentStation)
+
+		control = make(chan string)
+		wg.Add(1)
+		go func() {
+			err := playStream(selectedStation.URL, control, &wg)
 			if err != nil {
-				fmt.Println("Ошибка ввода команды.")
-				continue
+				fmt.Fprintln(os.Stderr, "Ошибка во время прослушивания:", err)
 			}
-			switch cmd {
-			case "pause", "resume", "stop":
-				control <- cmd
-				if cmd == "stop" {
-					wg.Wait()
-					exitInnerLoop = true
-				}
-			case "change":
-				control <- "stop"
-				wg.Wait()
-				exitInnerLoop = true
-			default:
-				fmt.Println("Неизвестная команда.")
+		}()
+
+		info.SetText(fmt.Sprintf("[green]Играет: %s", selectedStation.Name))
+		app.SetRoot(flex, true).SetFocus(list)
+	})
+
+	go updateStationsPeriodically(10*time.Minute, &stations)
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if control != nil {
+			switch event.Key() {
+			case tcell.KeyCtrlZ:
+				control <- "volume_up"
+				updateEqualizer(equalizer, volume.Volume, currentStation)
+			case tcell.KeyCtrlX:
+				control <- "volume_down"
+				updateEqualizer(equalizer, volume.Volume, currentStation)
+			case tcell.KeyLeft:
+				speaker.Lock()
+				volume.Volume = -2
+				speaker.Unlock()
+				updateEqualizer(equalizer, volume.Volume, currentStation)
+			case tcell.KeyRight:
+				speaker.Lock()
+				volume.Volume = 2
+				speaker.Unlock()
+				updateEqualizer(equalizer, volume.Volume, currentStation)
 			}
 		}
+
+		switch event.Rune() {
+		case 'S', 's':
+			index := list.GetCurrentItem()
+			if index >= 0 && index < len(stations) {
+				favorites = append(favorites, stations[index])
+				saveStationsToFavorites(favorites)
+				favoritesList.AddItem(stations[index].Name, "", rune('0'+len(favorites)%10), nil)
+				info.SetText(fmt.Sprintf("[yellow]Станция добавлена в избранное: %s", stations[index].Name))
+			}
+		case 'D', 'd':
+			index := favoritesList.GetCurrentItem()
+			if index >= 0 && index < len(favorites) {
+				favorites = append(favorites[:index], favorites[index+1:]...)
+				saveStationsToFavorites(favorites)
+				favoritesList.Clear()
+				for i, station := range favorites {
+					favoritesList.AddItem(station.Name, "", rune('0'+i%10), nil)
+				}
+				info.SetText("[yellow]Станция удалена из избранного")
+			}
+		case 'M', 'm':
+			app.SetRoot(favoritesList, true).SetFocus(favoritesList)
+		case 'B', 'b':
+			app.SetRoot(flex, true).SetFocus(list)
+		}
+
+		return event
+	})
+
+	if err := app.SetRoot(flex, true).Run(); err != nil {
+		panic(err)
 	}
 }
